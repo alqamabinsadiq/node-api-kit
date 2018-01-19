c5e36d58bbf105485b7af8fa6143f3dcd08b7b77
alqamabinsadiq <alqamabinsadiq@gmail.com>
2018-01-20 00:24:31 +0500

    add user model and user router in it's own feature sub folder

---
+++ features/users/userRouter.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var passport = require('passport');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var authenticate = require('../authenticate');
+const cors = require('./cors');
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+/* GET users listing. */
+// Only few origins can use this operation.
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+  User.find({}, (err, user) => {
+    if (err) throw err;
+    res.json(user);
+  });
+});
+
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        if (req.body.firstname)
+          user.firstname = req.body.firstname;
+        if (req.body.lastname)
+          user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' });
+          });
+        });
+      }
+    });
+});
+
+// We don't need to handle the error here because it is already taken care of by 
+// passport local mongoose plugin.
+
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
+
+router.get('/logout', cors.corsWithOptions, (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+});
+
+
+module.exports = router;
