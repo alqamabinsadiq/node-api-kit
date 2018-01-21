ea5afe892c9281b1d196413ac0f2e604725b4b92
alqamabinsadiq <alqamabinsadiq@gmail.com>
2018-01-21 11:42:41 +0500

    Move the registration logic from router to userController.js

---
+++ features/users/userController.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const User = require('./userModel');
+const authenticate = require('../../authenticate');
+
+exports.register = (req, res) => {
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        if (req.body.firstname)
+          user.firstname = req.body.firstname;
+        if (req.body.lastname)
+          user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, message: 'Registration Successful!' });
+          });
+        });
+      }
+    });
+}
--- features/users/userRouter.js
+++ features/users/userRouter.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 var User = require('./userModel');
 var authenticate = require('../../authenticate');
 const cors = require('../../server/cors');
-
+const userCtrl = require('./userController');
 var router = express.Router();
 router.use(bodyParser.json());
 
@@ -17,35 +17,8 @@ router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.veri
   });
 });
 
-router.post('/signup', cors.corsWithOptions, (req, res, next) => {
-  User.register(new User({ username: req.body.username }),
-    req.body.password, (err, user) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ err: err });
-      }
-      else {
-        if (req.body.firstname)
-          user.firstname = req.body.firstname;
-        if (req.body.lastname)
-          user.lastname = req.body.lastname;
-        user.save((err, user) => {
-          if (err) {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({ err: err });
-            return;
-          }
-          passport.authenticate('local')(req, res, () => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({ success: true, status: 'Registration Successful!' });
-          });
-        });
-      }
-    });
-});
+// Registers User.
+router.post('/signup', cors.corsWithOptions, userCtrl.register);
 
 // We don't need to handle the error here because it is already taken care of by 
 // passport local mongoose plugin.
